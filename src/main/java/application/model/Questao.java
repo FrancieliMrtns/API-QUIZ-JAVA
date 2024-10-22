91309ca689e59e342866936e05cf639e6e4772b0
Francieli Martins <fmartinsPOW777@gmail.com>
2024-10-22 17:55:48 +0000

    22/11/24

---
+++ src/main/java/application/model/Questao.java
@@ -0,0 +1,36 @@
+package application.model;
+
+import application.record.QuestaoDTO;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "questoes")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Questao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String enunciado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_categoria")
+    private Categoria categoria;
+    
+    public Questao(QuestaoDTO questao) {
+        this.id = questao.id();
+        this.enunciado = questao.enunciado();
+    }
+}
